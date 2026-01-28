4d98540767bc5be42e5370cd22ac71e08cdaf2bf
Ibrahim Munaser <ibrahimmunaser@gmail.com>
2026-01-27 19:03:11 -0500

    Add white background container for footer logo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
               href="/"
               className="flex items-center"
             >
-              <div className="relative h-16 w-auto" style={{ width: '220px' }}>
+              <div className="relative h-16 w-auto bg-white rounded-lg p-2" style={{ width: '220px' }}>
                 <Image
                   src="/Images/logo.png"
                   alt={`${clinicInfo.name} Logo`}
